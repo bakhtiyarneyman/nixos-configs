08d92fdf485535cb8a6b2e3982eac7697676e8a7
Bakhtiyar Neyman <b.neymanov@gmail.com>
2020-08-03 19:02:28 -0700

    New version of z.

--- pkgs/fish/z.nix
+++ pkgs/fish/z.nix
@@ -7,14 +7,14 @@ stdenv.mkDerivation rec {
     owner = "jethrokuan";
     repo = name;
     rev = "${version}";
-    sha256 = "0c5i7sdrsp0q3vbziqzdyqn4fmp235ax4mn4zslrswvn8g3fvdyh";
+    sha256 = "0kykhan9rdzy8anif5jp1iv3djrakhwk2arll3k93vaxm3np0gfm";
   };
 
   installPhase = import ./installPhase.nix;
 
   meta = with stdenv.lib; {
     description = "z tracks the directories you visit.";
-    homepage = https://github.com/jethrokuan/fzf;
+    homepage = https://github.com/jethrokuan/z;
     license = licenses.mit;
     platforms = platforms.linux;
   };
