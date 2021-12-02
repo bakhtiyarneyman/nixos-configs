27814840187eee4935b252220d697d773e840df1
Bakhtiyar Neyman <bakhtiyarneyman@gmail.com>
2021-12-02 01:08:40 -0800

    Fix the locker.

--- prettyLock.nix
+++ prettyLock.nix
@@ -7,44 +7,44 @@ writeShellScriptBin "prettyLock" ''
     --force-clock \
     --keylayout \
     --datestr="%A, %Y-%m-%d" \
-    --veriftext="Authenticating..." \
-    --wrongtext="Try again." \
-    --greetertext="bakhtiyarneyman@gmail.com" \
-    --noinputtext="" \
-    --indpos="w/2:h/2" \
-    --timepos="ix+100:iy" \
-    --datepos="tx:ty+50" \
-    --layoutpos="ix-100:iy+8" \
-    --greeterpos="ix:iy+300" \
-    --verifpos="ix:iy-150" \
-    --wrongpos="ix:iy-150" \
-    --modifpos="ix:iy-100" \
-    --timesize=50 \
-    --datesize=20 \
-    --greetersize=16 \
-    --layoutsize=16 \
+    --verif-text="Authenticating..." \
+    --wrong-text="Try again." \
+    --greeter-text="bakhtiyarneyman@gmail.com" \
+    --noinput-text="" \
+    --ind-pos="w/2:h/2" \
+    --time-pos="ix+100:iy" \
+    --date-pos="tx:ty+50" \
+    --layout-pos="ix-100:iy+8" \
+    --greeter-pos="ix:iy+300" \
+    --verif-pos="ix:iy-150" \
+    --wrong-pos="ix:iy-150" \
+    --modif-pos="ix:iy-100" \
+    --time-size=50 \
+    --date-size=20 \
+    --greeter-size=16 \
+    --layout-size=16 \
     --time-align=1 \
     --date-align=1 \
     --layout-align=2 \
     --greeter-align=0 \
     --color="00000020" \
-    --keyhlcolor="6060f0ff" \
-    --bshlcolor="c678ddff" \
-    --separatorcolor="00000000" \
-    --verifcolor="c678ddff" \
-    --wrongcolor="e06c75ff" \
-    --ringcolor="ffffffff" \
-    --ringwrongcolor="e06c75ff" \
-    --ringvercolor="c678ddff" \
-    --insidecolor="00000000" \
-    --insidevercolor="00000000" \
-    --insidewrongcolor="00000000" \
+    --keyhl-color="6060f0ff" \
+    --bshl-color="c678ddff" \
+    --separator-color="00000000" \
+    --verif-color="c678ddff" \
+    --wrong-color="e06c75ff" \
+    --ring-color="ffffffff" \
+    --ringwrong-color="e06c75ff" \
+    --ringver-color="c678ddff" \
+    --inside-color="00000000" \
+    --insidever-color="00000000" \
+    --insidewrong-color="00000000" \
+    --time-color="ffffffff" \
+    --date-color="ffffffff" \
+    --layout-color="ffffffff" \
+    --greeter-color="ffffffff" \
     --line-uses-inside \
     --ring-width=3 \
-    --timecolor="ffffffff" \
-    --datecolor="ffffffff" \
-    --layoutcolor="ffffffff" \
-    --greetercolor="ffffffff" \
     --tiling \
     --redraw-thread
 ''
