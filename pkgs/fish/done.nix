4aea7b10a21a2160614b8ec599f445e3a0bca2f2
Bakhtiyar Neyman <bakhtiyarneyman@gmail.com>
2021-12-22 18:53:19 -0800

    Update done.

--- pkgs/fish/done.nix
+++ pkgs/fish/done.nix
@@ -1,13 +1,13 @@
 { stdenv, lib, fetchFromGitHub }:
 stdenv.mkDerivation rec {
   name = "done";
-  version = "1.14.10";
+  version = "1.16.5";
 
   src = fetchFromGitHub {
     owner = "franciscolourenco";
     repo = name;
     rev = "${version}";
-    sha256 = "1fn4q2clm0n9agb9f2vx1zj3g785kfjyyfdr2w3zzmsjaa8kcxqr";
+    sha256 = "1m11nsdmd82x0l3i8zqw8z3ba77nxanrycv93z25rmghw1wjyk0k";
   };
 
   installPhase = import ./installPhase.nix;
