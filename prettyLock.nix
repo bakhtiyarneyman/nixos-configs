74bc5dde96aec809420dabb6c5e65331da4e7345
Bakhtiyar Neyman <bakhtiyarneyman@gmail.com>
2023-01-27 13:51:05 -0800

    Switch to swaylock.

--- prettyLock.nix
+++ prettyLock.nix
@@ -1,51 +1,22 @@
 { config, pkgs, ... }:
 with pkgs;
 writeShellScriptBin "prettyLock" ''
-  ${i3lock-color}/bin/i3lock-color \
-    --radius=40 \
-    --indicator \
-    --force-clock \
-    --keylayout \
-    --datestr="%A, %Y-%m-%d" \
-    --verif-text="Authenticating..." \
-    --wrong-text="Try again." \
-    --greeter-text="bakhtiyarneyman@gmail.com" \
-    --noinput-text="" \
-    --ind-pos="w/2:h/2" \
-    --time-pos="ix+100:iy" \
-    --date-pos="tx:ty+50" \
-    --layout-pos="ix-100:iy+8" \
-    --greeter-pos="ix:iy+300" \
-    --verif-pos="ix:iy-150" \
-    --wrong-pos="ix:iy-150" \
-    --modif-pos="ix:iy-100" \
-    --time-size=50 \
-    --date-size=20 \
-    --greeter-size=16 \
-    --layout-size=16 \
-    --time-align=1 \
-    --date-align=1 \
-    --layout-align=2 \
-    --greeter-align=0 \
+  ${swaylock}/bin/swaylock \
+    --indicator-radius=40 \
+    --show-failed-attempts \
     --image=${./wallpaper.jpg} \
     --color="000000ff" \
-    --keyhl-color="6060f0ff" \
-    --bshl-color="c678ddff" \
+    --key-hl-color="6060f0ff" \
+    --bs-hl-color="c678ddff" \
     --separator-color="00000000" \
-    --verif-color="c678ddff" \
-    --wrong-color="e06c75ff" \
+    --text-ver-color="c678ddff" \
+    --text-wrong-color="e06c75ff" \
     --ring-color="ffffffff" \
-    --ringwrong-color="e06c75ff" \
-    --ringver-color="c678ddff" \
+    --ring-wrong-color="e06c75ff" \
+    --ring-ver-color="c678ddff" \
     --inside-color="00000000" \
-    --insidever-color="00000000" \
-    --insidewrong-color="00000000" \
-    --time-color="ffffffff" \
-    --date-color="ffffffff" \
-    --layout-color="ffffffff" \
-    --greeter-color="ffffffff" \
+    --inside-ver-color="00000000" \
+    --inside-wrong-color="00000000" \
     --line-uses-inside \
-    --ring-width=3 \
-    --tiling \
-    --redraw-thread
+    --indicator-thickness=3
 ''
