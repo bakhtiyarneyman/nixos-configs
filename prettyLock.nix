58efb0584f717b7f6ad6a341706ceb7e21f1dea3
Bakhtiyar Neyman <bakhtiyarneyman@gmail.com>
2023-01-31 10:17:06 -0800

    Improve colors of swaylock.

--- prettyLock.nix
+++ prettyLock.nix
@@ -11,12 +11,16 @@ writeShellScriptBin "prettyLock" ''
     --separator-color="00000000" \
     --text-ver-color="c678ddff" \
     --text-wrong-color="e06c75ff" \
+    --text-clear-color="56b6c2ff" \
     --ring-color="ffffffff" \
     --ring-wrong-color="e06c75ff" \
     --ring-ver-color="c678ddff" \
+    --ring-clear-color="56b6c2ff" \
     --inside-color="00000000" \
     --inside-ver-color="00000000" \
     --inside-wrong-color="00000000" \
+    --inside-clear-color="00000000" \
+    --layout-bg-color="00000000" \
     --line-uses-inside \
     --indicator-thickness=3
 ''
