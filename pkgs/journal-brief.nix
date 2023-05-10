8de51693baf8c877550780b0dcd782f92289bfb0
Bakhtiyar Neyman <bakhtiyarneyman@gmail.com>
2023-05-10 14:51:06 -0700

    Package journal-brief.

---
+++ pkgs/journal-brief.nix
@@ -0,0 +1,31 @@
+# Nix package for twaugh/journal-brief.
+{ lib
+, stdenv
+, fetchFromGitHub
+, python3Packages
+}: python3Packages.buildPythonPackage rec {
+
+  pname = "journal-brief";
+  version = "1.1.8";
+  src = fetchFromGitHub {
+    owner = "twaugh";
+    repo = "journal-brief";
+    rev = "v${version}";
+    sha256 = "Q0ydbIwn0w5rnZ4o1k9/XZLHHczIxvYIJvUscBAR120=";
+  };
+
+  propagatedBuildInputs = with python3Packages; [
+    systemd
+    pyyaml
+  ];
+
+  checkInputs = with python3Packages; [
+    pytest
+    flexmock
+    pytest-mock
+  ];
+
+  checkPhase = ''
+    pytest -k 'not test_login and not test_systemd'
+  '';
+}
