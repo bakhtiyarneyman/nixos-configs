c17548da1b5c7953912010d1803bf4205ad64aca
Bakhtiyar Neyman <bakhtiyarneyman@gmail.com>
2023-05-31 16:43:38 -0700

    Change untrusted password hash.

--- modules/untrusted.nix
+++ modules/untrusted.nix
@@ -2,7 +2,7 @@
   config = {
     users.users =
       let
-        hashedPassword = "$6$du.QjcXi8kM4UnuK$IRSujrQ2p8xPAQawatjWypo/OK2Sr3Em5orJYEjTjebzgiIwY4fyubn/F3tRbfzJLjpt9Dp3F/0gSJTN0fyrF/"; in
+        hashedPassword = "$6$8/ZCmPvSRiRnXlJN$nCS1B6KlgAlHdh1P5t0iRmmTT2vzLlm5YF9eLiYGvHwDCNXK0g1737P5yazyZPHOYWcjQUizGP2/92c3TILNx1"; in
       {
         root = { inherit hashedPassword; };
         bakhtiyar = { inherit hashedPassword; };
