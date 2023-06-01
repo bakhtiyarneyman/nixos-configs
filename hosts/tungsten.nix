c1ba3b354242c021e9992dbb4ad9a2cba27ddae0
Bakhtiyar Neyman <bakhtiyarneyman@gmail.com>
2023-06-01 15:24:58 -0700

    Add github key to authorized keys on untrusted machines.

--- hosts/tungsten.nix
+++ hosts/tungsten.nix
@@ -47,6 +47,7 @@
   };
 
   users.users.bakhtiyar.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGxoBwt5zviLpPomH5vHq0OQzN/G9dMKmyq+2y91xkRe github@1password"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDsGRMyBB18Gnhf5Igw/w5rbm6ks49TPZ2wY7iXKKh2L bakhtiyar@iron"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICT17FwJcNp9/YMx73tOakZutUtEbcjct4YPCywWsDL7 bakhtiyar@kevlar"
   ];
