9971fa514bd90b078277635415e179b800bf2b54
Bakhtiyar Neyman <bakhtiyarneyman@gmail.com>
2023-11-15 13:18:46 -0800

    Remove the sensitive dataset.

--- hosts/iron.nix
+++ hosts/iron.nix
@@ -137,10 +137,6 @@ in
               device = "slow/root/personal/video";
               fsType = "zfs";
             };
-            "/home/bakhtiyar/sensitive" = {
-              device = "fast/nixos/home/sensitive";
-              fsType = "zfs";
-            };
             "/home/bakhtiyar/warehouse" = {
               device = "slow/root/warehouse";
               fsType = "zfs";
