2b4a98e96acecb8d51648046cb3cb7cc8263d83b
Bakhtiyar Neyman <bakhtiyarneyman@gmail.com>
2024-02-03 18:37:04 -0800

    Isolate the network of neurasium container.

--- mixins/neurasium.nix
+++ mixins/neurasium.nix
@@ -1,112 +1,134 @@
 {
-  pkgs,
   config,
+  hostName,
+  lib,
+  pkgs,
   ...
 }: let
+  hostCfg = config;
+  containerName = "neurasium";
+  agentName = "builder";
+  userName = "buildkite-agent-${agentName}";
   uid = 986;
   gid = 982;
 in {
-  containers.neurasium = {
-    autoStart = true;
+  config = {
+    containers."${containerName}" = {
+      autoStart = true;
 
-    bindMounts = {
-      "/secrets/buildkite.pat".hostPath = "/etc/nixos/secrets/buildkite.pat";
-      "/secrets/buildkite.token".hostPath = "/etc/nixos/secrets/buildkite.token";
-    };
+      bindMounts = {
+        "/secrets/buildkite.pat".hostPath = "/etc/nixos/secrets/buildkite.pat";
+        "/secrets/buildkite.token".hostPath = "/etc/nixos/secrets/buildkite.token";
+      };
 
-    ephemeral = false;
-
-    config = let
-      cfg = config.containers.neurasium.config;
-    in {
-      networking = {
-        firewall = {
-          enable = true;
-          logRefusedConnections = true;
-          checkReversePath = "loose";
+      config = let
+        containerCfg = hostCfg.containers."${containerName}".config;
+      in {
+        networking = {
+          firewall = {
+            enable = true;
+            logRefusedConnections = true;
+            checkReversePath = "loose";
+          };
+          hostName = "${hostName}-${containerName}";
         };
-      };
 
-      nix = {
-        extraOptions = ''
-          extra-experimental-features = nix-command flakes
-        '';
-        settings.cores = 8;
-        settings.max-jobs = 1;
-        package = pkgs.nix;
-      };
+        nix = {
+          extraOptions = ''
+            extra-experimental-features = nix-command flakes
+          '';
+          settings.cores = 8;
+          settings.max-jobs = 1;
+          package = pkgs.nix;
+        };
 
-      programs = {
-        git = {
-          enable = true;
-          config = {
-            credential.helper = "store";
+        programs = {
+          git = {
+            enable = true;
+            config = {
+              credential.helper = "store";
+            };
+          };
+        };
+
+        services = {
+          gerrit = {
+            enable = true;
+            serverId = "iron-${containerName}";
+          };
+
+          buildkite-agents."${agentName}" = {
+            hooks.environment = ''
+              export PAGER=
+            '';
+            runtimePackages = [
+              pkgs.bash
+              pkgs.direnv
+              pkgs.git
+              pkgs.gnugrep
+              pkgs.gnutar
+              pkgs.gzip
+              (pkgs.writeShellScriptBin "nix-env" ''
+                exec ${pkgs.nix}/bin/nix-env "$@"
+              '')
+              (pkgs.writeShellScriptBin "nix-store" ''
+                exec ${pkgs.nix}/bin/nix-store "$@"
+              '')
+              (pkgs.writeShellScriptBin "nix" ''
+                exec ${pkgs.nix}/bin/nix --print-build-logs "$@"
+              '')
+            ];
+            shell = "${pkgs.bash}/bin/bash -euo pipefail -c";
+            tokenPath = "/secrets/buildkite.token";
+          };
+        };
+
+        system.stateVersion = "23.11";
+
+        systemd.services."${userName}" = {
+          preStart = ''
+            set -euo pipefail
+            export BUILDKITE_PAT=$(cat /secrets/buildkite.pat)
+            echo \
+              "https://neurasium-buildkite-agent:$BUILDKITE_PAT@github.com" \
+              > "${containerCfg.services.buildkite-agents.${agentName}.dataDir}/.git-credentials"
+          '';
+        };
+
+        users = {
+          users."${userName}" = {
+            inherit uid;
+          };
+          groups."${userName}" = {
+            inherit gid;
           };
         };
       };
 
-      services = {
-        gerrit = {
-          enable = true;
-          serverId = "iron-neurasium";
-        };
-
-        buildkite-agents.neurasium = {
-          hooks.environment = ''
-            export PAGER=
-          '';
-          runtimePackages = [
-            pkgs.bash
-            pkgs.direnv
-            pkgs.git
-            pkgs.gnugrep
-            pkgs.gnutar
-            pkgs.gzip
-            (pkgs.writeShellScriptBin "nix-env" ''
-              exec ${pkgs.nix}/bin/nix-env "$@"
-            '')
-            (pkgs.writeShellScriptBin "nix-store" ''
-              exec ${pkgs.nix}/bin/nix-store "$@"
-            '')
-            (pkgs.writeShellScriptBin "nix" ''
-              exec ${pkgs.nix}/bin/nix --print-build-logs "$@"
-            '')
-          ];
-          shell = "${pkgs.bash}/bin/bash -euo pipefail -c";
-          tokenPath = "/secrets/buildkite.token";
-        };
-      };
-
-      system.stateVersion = "23.11";
-
-      systemd.services.buildkite-agent-neurasium = {
-        preStart = ''
-          set -euo pipefail
-          export BUILDKITE_PAT=$(cat /secrets/buildkite.pat)
-          echo \
-            "https://neurasium-buildkite-agent:$BUILDKITE_PAT@github.com" \
-            > "${cfg.services.buildkite-agents.neurasium.dataDir}/.git-credentials"
-        '';
-      };
-
-      users = {
-        users.buildkite-agent-neurasium = {
-          inherit uid;
-        };
-        groups.buildkite-agent-neurasium = {
-          inherit gid;
-        };
-      };
+      ephemeral = false;
+      # hostBridge = bridgeName;
+      hostAddress = "192.168.100.10";
+      localAddress = "192.168.100.11";
+      privateNetwork = true;
     };
-  };
 
-  users = {
-    users.buildkite-agent-neurasium = {
-      inherit uid;
-      group = "buildkite-agent-neurasium";
+    networking = {
+      nat = {
+        internalInterfaces = ["ve-+"];
+        enable = true;
+        enableIPv6 = true;
+      };
+      useDHCP = lib.mkDefault true;
     };
-    groups.buildkite-agent-neurasium = {
-      inherit gid;
+
+    users = {
+      users.${userName} = {
+        inherit uid;
+        group = userName;
+      };
+      groups.${userName} = {
+        inherit gid;
+      };
     };
   };
 }
