82d443032ca4950b1ae5cb28d4bfabac17a267f7
Bakhtiyar Neyman <bakhtiyarneyman@gmail.com>
2024-10-08 18:14:20 -0700

    Enable firewall for local network game transfers in steam.

--- mixins/gui.nix
+++ mixins/gui.nix
@@ -374,8 +374,9 @@ in {
       };
       steam = {
         enable = true;
-        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        localNetworkGameTransfers.openFirewall = true;
       };
 
       _1password-gui = {
