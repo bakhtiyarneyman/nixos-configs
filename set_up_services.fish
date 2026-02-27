846e49cd030c03752870ba229ae4e7affd7a46a9
Bakhtiyar Neyman <bakhtiyarneyman@gmail.com>
2026-02-27 02:18:45 -0800

    Update the qbittorrent service.

--- set_up_services.fish
+++ set_up_services.fish
@@ -5,6 +5,6 @@ sudo tailscale serve --https=443 --service=svc:jellyfin localhost:8096
 sudo tailscale serve --https=443 --service=svc:jellyseerr localhost:5055
 sudo tailscale serve --https=443 --service=svc:ntopng localhost:4256
 sudo tailscale serve --https=443 --service=svc:prowlarr localhost:9696
-sudo tailscale serve --https=443 --service=svc:qbittorrent localhost:8080
+sudo tailscale serve --https=443 --service=svc:qbittorrent https+insecure://localhost:8097
 sudo tailscale serve --https=443 --service=svc:radarr localhost:7878
 sudo tailscale serve --https=443 --service=svc:sonarr localhost:8989
